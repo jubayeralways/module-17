f15f7eb4206f0655571db8f93d155bfc708d104a
jubayeralways <mdjubayerhossain7@gmail.com>
2026-02-01 17:32:46 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,46 @@
+// function brushTeeth(){
+//     console.log('pick up the brush');
+//     console.log('add tooth-paste');
+
+    
+// }
+// brushTeeth();
+
+/////
+// function doubleIt (number){
+//     const doubled = number * 2 ;
+//     console.log(number, doubled);
+    
+// }
+// console.log('i will call the funtion');
+// doubleIt(15);
+// console.log('---------------');
+// doubleIt(388);
+// console.log('---------------');
+// doubleIt(1010);
+/////////
+
+// function difference (nam1 , nam2){
+//     const diff = nam1 - nam2;
+//      console.log(diff);
+     
+// }
+// const fatherage = 40;
+// const myage = 20; 
+// difference(fatherage, myage);
+
+
+////////
+
+function tentimes (number){
+    const result = number / 3;
+    return result;
+
+}
+
+tentimes(33);
+const output = tentimes(33);
+console.log( 'output' , output);
+
+
+
